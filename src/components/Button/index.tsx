f37798c479c005d3a4e54737cf54ab8744e24edd
wangxingkang <wang_xingkang@qq.com>
2025-03-14 17:17:43 +0800

    chore: update button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,12 @@
-export function Button() {
+interface ButtonProps {
+  text?: string;
+}
+
+export function Button(props: ButtonProps) {
   return (
     <div style={{ color: 'red' }}>
       我是按钮
+      {props.text}
     </div>
-  )
+  );
 }
