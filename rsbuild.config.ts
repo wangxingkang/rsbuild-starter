80f2250d9c962a6b360cbe15f11536475bea9f70
wangxingkang <wang_xingkang@qq.com>
2025-03-14 15:16:22 +0800

    chore: add rsdoctor

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,8 @@
+import process from 'node:process';
 import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
 import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack';
 
 export default defineConfig({
@@ -26,6 +28,7 @@ export default defineConfig({
   tools: {
     rspack: {
       plugins: [
+        process.env.RSDOCTOR === 'true' && new RsdoctorRspackPlugin({}),
         TanStackRouterRspack({
           target: 'react',
           autoCodeSplitting: true,
